c234c1eb3723f57c01207cd1649d5669304d4ace
Evgeniy Talko <etalko@gmail.com>
2018-02-13 16:05:26 +0300

    some changes

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,566 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "homework_Homepage_0_document_0_phone.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_1_viewport_0_phone.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_1_viewport_0_phone.png",
+        "selector": "viewport",
+        "fileName": "homework_Homepage_1_viewport_0_phone.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_2_header_0_phone.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_2_header_0_phone.png",
+        "selector": ".header",
+        "fileName": "homework_Homepage_2_header_0_phone.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_3_hero_0_phone.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_3_hero_0_phone.png",
+        "selector": ".hero",
+        "fileName": "homework_Homepage_3_hero_0_phone.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_4_about-left_0_phone.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_4_about-left_0_phone.png",
+        "selector": ".about-left",
+        "fileName": "homework_Homepage_4_about-left_0_phone.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_5_about-right_0_phone.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_5_about-right_0_phone.png",
+        "selector": ".about-right",
+        "fileName": "homework_Homepage_5_about-right_0_phone.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_6_map-contacts_0_phone.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_6_map-contacts_0_phone.png",
+        "selector": ".map-contacts",
+        "fileName": "homework_Homepage_6_map-contacts_0_phone.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "homework_Homepage_0_document_1_tablet.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_1_viewport_1_tablet.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_1_viewport_1_tablet.png",
+        "selector": "viewport",
+        "fileName": "homework_Homepage_1_viewport_1_tablet.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_2_header_1_tablet.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_2_header_1_tablet.png",
+        "selector": ".header",
+        "fileName": "homework_Homepage_2_header_1_tablet.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_3_hero_1_tablet.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_3_hero_1_tablet.png",
+        "selector": ".hero",
+        "fileName": "homework_Homepage_3_hero_1_tablet.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_4_about-left_1_tablet.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_4_about-left_1_tablet.png",
+        "selector": ".about-left",
+        "fileName": "homework_Homepage_4_about-left_1_tablet.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_5_about-right_1_tablet.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_5_about-right_1_tablet.png",
+        "selector": ".about-right",
+        "fileName": "homework_Homepage_5_about-right_1_tablet.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_6_map-contacts_1_tablet.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_6_map-contacts_1_tablet.png",
+        "selector": ".map-contacts",
+        "fileName": "homework_Homepage_6_map-contacts_1_tablet.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_0_document_2_desktop.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "homework_Homepage_0_document_2_desktop.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.06",
+          "analysisTime": 152
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_1_viewport_2_desktop.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_1_viewport_2_desktop.png",
+        "selector": "viewport",
+        "fileName": "homework_Homepage_1_viewport_2_desktop.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_2_header_2_desktop.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_2_header_2_desktop.png",
+        "selector": ".header",
+        "fileName": "homework_Homepage_2_header_2_desktop.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_3_hero_2_desktop.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_3_hero_2_desktop.png",
+        "selector": ".hero",
+        "fileName": "homework_Homepage_3_hero_2_desktop.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_4_about-left_2_desktop.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_4_about-left_2_desktop.png",
+        "selector": ".about-left",
+        "fileName": "homework_Homepage_4_about-left_2_desktop.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_5_about-right_2_desktop.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_5_about-right_2_desktop.png",
+        "selector": ".about-right",
+        "fileName": "homework_Homepage_5_about-right_2_desktop.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_6_map-contacts_2_desktop.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_6_map-contacts_2_desktop.png",
+        "selector": ".map-contacts",
+        "fileName": "homework_Homepage_6_map-contacts_2_desktop.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_0_document_3_desktop-hd.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_0_document_3_desktop-hd.png",
+        "selector": "document",
+        "fileName": "homework_Homepage_0_document_3_desktop-hd.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_1_viewport_3_desktop-hd.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_1_viewport_3_desktop-hd.png",
+        "selector": "viewport",
+        "fileName": "homework_Homepage_1_viewport_3_desktop-hd.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_2_header_3_desktop-hd.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_2_header_3_desktop-hd.png",
+        "selector": ".header",
+        "fileName": "homework_Homepage_2_header_3_desktop-hd.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_3_hero_3_desktop-hd.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_3_hero_3_desktop-hd.png",
+        "selector": ".hero",
+        "fileName": "homework_Homepage_3_hero_3_desktop-hd.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_4_about-left_3_desktop-hd.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_4_about-left_3_desktop-hd.png",
+        "selector": ".about-left",
+        "fileName": "homework_Homepage_4_about-left_3_desktop-hd.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_5_about-right_3_desktop-hd.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_5_about-right_3_desktop-hd.png",
+        "selector": ".about-right",
+        "fileName": "homework_Homepage_5_about-right_3_desktop-hd.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/homework_Homepage_6_map-contacts_3_desktop-hd.png",
+        "test": "../bitmaps_test/20180213-160002/homework_Homepage_6_map-contacts_3_desktop-hd.png",
+        "selector": ".map-contacts",
+        "fileName": "homework_Homepage_6_map-contacts_3_desktop-hd.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
