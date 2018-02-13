ffadcb033ac2499f3856f0f24251a45a74420d82
Evgeniy Talko <etalko@gmail.com>
2018-02-13 16:06:15 +0300

    removed old tasks

--- gulp_old/tasks/copy.fonts.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function() {
-  $.gulp.task('copy:fonts', function() {
-    return $.gulp.src($.path.src + '/fonts/**/*.*', { since: $.gulp.lastRun('copy:fonts') })
-      .pipe($.gulp.dest($.path.dist + '/fonts'))
-      .pipe($.gp.size({title: "Fonts"}));
-  });
-};
