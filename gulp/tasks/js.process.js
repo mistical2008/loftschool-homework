c04978ccbc23e7de188ac64f5deff385663e2156
Evgeniy Talko <etalko@gmail.com>
2018-02-16 13:44:59 +0300

    Started works page. Too many time due indentation error in svg!

--- gulp/tasks/js.process.js
+++ gulp/tasks/js.process.js
@@ -3,9 +3,9 @@
 module.exports = function() {
   $.gulp.task('js:process', function() {
     return $.gulp.src($.path.app)
-      .pipe($.gp.sourcemaps.init())
+      // .pipe($.gp.sourcemaps.init())
       .pipe($.gp.concat('main.js'))
-      .pipe($.gp.sourcemaps.write())
+      // .pipe($.gp.sourcemaps.write())
       .pipe($.gulp.dest($.path.src + '/js'))
       .pipe($.gp.size({title: "Scripts"}));
   })
